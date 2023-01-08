f225b363cfe90ffd40a5be62c7e826035c379cac
KIMEUNCHONG <sv5506829sv@gmail.com>
2023-01-08 18:02:50 +0900

    clear

--- Python Basic 100/6098 성실한 개미/main.py
+++ Python Basic 100/6098 성실한 개미/main.py
@@ -4,24 +4,38 @@ for i in range(10):
     tmp = list(map(int, input().split()))
     table.append(tmp)
     
-    
-table[1][1] = 9 # 시작 위치
-    
-for y in range(1, 10):
-    for x in range(1, 10):
-        if table[y][x] == 2:    # 먹이를 찾으면
-            break
-        elif (table[y][x+1] == 1) or (table[y+1][x] == 1):  # 이동이 불가능하면
-            break
-        elif table[9][x] == 1:  # 맨 아래 오른쪽에 도착하면
+
+y = 1
+x = 1
+flag = False
+
+while y < 10:
+    while x < 10:   # 오른쪽으로 이동
+        
+        if (table[y][x] == 2) or (y == 8 and x == 8):    # 먹이를 찾으면 or 맨 아래 오른쪽에 도착하면
+            table[y][x] = 9     # 9로 표시
+            flag = True         # 이중 루프 탈출
             break
         
-        if table[y][x+1] != 1:  # 오른쪽으로 이동
-            table[y][x+1] = 9
-        elif table[y+1][x] != 1:  # 아래로 이동
-            table[y+1][x] = 9
+
+        if (table[y][x] == 1) and (table[y+1][x-1] == 1):  # 이동이 불가능하면
+            flag = True     # 이중 루프 탈출
+            break
         
-print()
-for i in range(10):
-    for j in range(10):
-        print(table[i][j])
+        if (table[y][x] == 1) and (table[y+1][x-1] != 1):  # 오른쪽으로 이동이 불가능하면
+            y = y + 1       # 아래로 이동
+            x = x - 1
+            continue
+        
+        table[y][x] = 9        # 방문 한 곳 9로 표시 
+        
+        x += 1
+    
+    if flag:    # 이중 루프 탈출
+        break
+    
+    y += 1
+    
+    
+for i in range(10):     # 결과 출력
+    print(*table[i])
